4bd27b26964c540bad3ed7a6510e88e000e6c9f9
Arris Moise <mikaelmoise@protonmail.com>
2020-07-21 15:12:36 -0400

    favicon

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -2,14 +2,14 @@
 <!DOCTYPE html>
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
     <head>
-        <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <meta charset="utf-8"/>
+        <meta name="viewport" content="width=device-width, initial-scale=1"/>
 
         <title>{{ env('APP_NAME') }}</title>
 
-        <link rel="icon" sizes="114x114" href="{{ URL::asset('images/favicon.png') }}" type="image/x-icon"/>
+        <link rel="icon" sizes="16x16" href="{{ asset('favicon.png') }}" />
 
-<link href='https://fonts.googleapis.com/css?family=Lato:300,400,700' rel='stylesheet' type='text/css'>
+<link href='https://fonts.googleapis.com/css?family=Lato:300,400,700' rel='stylesheet' type='text/css'/>
 <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css" integrity="sha384-9aIt2nRpC12Uk9gS9baDl411NQApFmC26EwAOH8WgZl5MYYxFfc+NcPb1dKGj7Sk" crossorigin="anonymous">
 
 

8148fc558e3d932b2e7713ae0f1936457efe6f8d
Arris Moise <mikaelmoise@protonmail.com>
2020-07-27 15:08:03 -0400

    background image

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -37,7 +37,7 @@
            <li class="nav-item dropdown">
 
 
-                <a id="navbarDropdown" style="color: white;" class="nav-link dropdown-toggle"  role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
+                <a id="navbarDropdown" style="color: white;" class="nav-link dropdown-toggle text-center"  role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
                                     {{ Auth::user()->name }} <span class="caret"></span>
                                 </a>
 
--- resources/views/notebooks/index.blade.php
+++ resources/views/notebooks/index.blade.php
@@ -14,7 +14,7 @@
 <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/js/bootstrap.min.js" integrity="sha384-OgVRvuATP1z7JjHLkuOU7Xw704+h835Lr+6QL9UvYjZE3Ipu6Tp75j7Bh/kR0JKI" crossorigin="anonymous"></script>
 
 </head>
-<body>
+<body style=" background-image: url('{{asset('img/stars.jpeg')}}');">
 <div id="app">
 	<nav class="navbar navbar-expand-lg navbar-light bg-light">
   <a class="navbar-brand" href="#">rocketdreamz</a>

480fc1a12ecd077c680737d3045f5c24986f30f5
Arris Moise <46009365+arrismo@users.noreply.github.com>
2020-07-28 13:21:35 -0400

    create blade

--- resources/views/notebooks/create.blade.php
+++ resources/views/notebooks/create.blade.php
@@ -5,9 +5,6 @@
 <head>
 	<title>rocketdreamz</title>
 
-
-
-<!-- 
 <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
 
 <script src="https://code.jquery.com/jquery-3.4.1.slim.min.js" integrity="sha384-J6qa4849blE2+poT4WnyKhv5vZF5SrPo0iEjwBvKU7imGFAV0wwj1yYfoRSJoZ+n" crossorigin="anonymous"></script>
@@ -17,7 +14,7 @@
 
 <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
 
--->
+
 
 <link href="{{ URL::asset('css/ncreate.css') }}" rel="stylesheet" type="text/css" >
 
@@ -54,7 +51,7 @@
     </ul>
   </div>
 
-<!-- 
+
 	<h1 style="text-align: center"> Create Notebook </h1>
 
 <form action="/notebooks" method="POST">
@@ -77,7 +74,7 @@
 
 </form>
 
--->
+
 
 
 
